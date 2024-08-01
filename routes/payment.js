9fd48e5a8806973208d550bbcbcb056ef8354e53
Wilson Bovea Cifuentes <wilsonboveacifuentes@MacBook-Air-de-Wilson.local>
2024-08-01 19:14:02 +0200

    modification route payment

---
+++ routes/payment.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+
+const stripe = require("stripe")(process.env.KEY_STRIPE);
+
+router.post("/payment", async (req, res) => {
+  try {
+    // On crée une intention de paiement
+    const paymentIntent = await stripe.paymentIntents.create({
+      // Montant de la transaction
+      amount: req.body.amount,
+      // Devise de la transaction
+      currency: "eur",
+      // Description du produit
+      description: req.body.title,
+    });
+    // On renvoie les informations de l'intention de paiement au client
+    res.json(paymentIntent);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+module.exports = router;
